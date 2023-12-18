ced581f87b5f1d578c523d85b1a6a24354f8cda4
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2023-12-18 15:41:13 +0100

    loo

--- routes/sellerRoute.js
+++ routes/sellerRoute.js
@@ -49,7 +49,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: function ( req, file, cb ) {
-        cb( null, path.join(__dirname, '../https://godswillsecom.onrender.com/public/images'));
+        cb( null, path.join(__dirname, 'https://godswillsecom.onrender.com/public/images'));
     },
     filename: function ( req, file, cb ) {
         cb( null, Date.now() + path.extname(file.originalname) );
